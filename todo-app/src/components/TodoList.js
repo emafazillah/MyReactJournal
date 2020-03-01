48e85889f3cd6d2c8a90c988707437d96a0f4781
emafazillah <gatsu2811@gmail.com>
2020-03-01 17:09:00 +0800

    Update TodoList.js

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -2,16 +2,8 @@
 
 // ...other imports
 import { connect } from "react-redux";
+import { getTodos } from "../redux/selectors";
 
 const TodoList = // ... UI component implementation
 
-const mapStateToProps = state => {
-  const { byIds, allIds } = state.todos || {};
-  const todos =
-    allIds && allIds.length
-      ? allIds.map(id => (byIds ? { ...byIds[id], id } : null))
-      : null;
-  return { todos };
-};
-
-export default connect(mapStateToProps)(TodoList);
+export default connect(state => ({ todos: getTodos(state) }))(TodoList);
