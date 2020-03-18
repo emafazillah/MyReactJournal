f929799d2435309b92b4d541ed4d8dbaf2428c79
emafazillah <gatsu2811@gmail.com>
2020-03-18 14:15:35 +0800

    Connecting VisibilityFilters component

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Todo from "./Todo";
 import { connect } from "react-redux";
-import { getTodos } from "../redux/selectors";
+// import { getTodos } from "../redux/selectors";
+import { getTodosByVisibilityFilter } from "../redux/selectors";
+import { VISIBILITY_FILTERS } from "../constants";
 
 const TodoList = ({ todos }) => (
   <ul className="todo-list">
@@ -13,13 +15,21 @@ const TodoList = ({ todos }) => (
   </ul>
 );
 
-const mapStateToProps = state => {
-  const { byIds, allIds } = state.todos || {};
-  const todos =
-    allIds && allIds.length
-      ? allIds.map(id => (byIds ? { ...byIds[id], id } : null))
-      : null;
-  return { todos };
-};
+// const mapStateToProps = state => {
+//   const { byIds, allIds } = state.todos || {};
+//   const todos =
+//     allIds && allIds.length
+//       ? allIds.map(id => (byIds ? { ...byIds[id], id } : null))
+//       : null;
+//   return { todos };
+// };
 
-export default connect(state => ({ todos: getTodos(state) }))(TodoList);
+// export default connect(state => ({ todos: getTodos(state) }))(TodoList);
+
+const mapStateToProps = state => {
+  const { visibilityFilter } = state
+  const todos = getTodosByVisibilityFilter(state, visibilityFilter)
+  return { todos }
+}
+
+export default connect(mapStateToProps)(TodoList);
