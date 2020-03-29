5544ad557f49ef9babb6d2e90843eee541e2743c
emafazillah <gatsu2811@gmail.com>
2020-03-29 13:24:57 +0800

    Add learn-promise

---
+++ learn-promise/index.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch');
+ 
+const lat = 37;
+const lon = 122;
+ 
+const sevenTimerTemp = 
+fetch(`http://www.7timer.info/bin/api.pl?lon=${lon}&lat=${lat}&product=astro&output=json`)
+.then(response => response.json())
+.then(response => response.dataseries[0].temp2m);
+ 
+const fccWeatherTemp =
+fetch(`https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lon}`)
+.then(response => response.json())
+.then(response => response.main.temp);
+ 
+Promise.all([sevenTimerTemp, fccWeatherTemp])
+.then(responses => {
+    let sum = 0;
+    let count = 0;
+    responses.forEach(response => {
+        sum += response;
+        count++;
+    });
+    return sum / count;
+})
+.then(average => console.log(`Average of reported temperatures is: ${average} C`))
+.catch(() => console.log(`One of the APIs returned an error`));
